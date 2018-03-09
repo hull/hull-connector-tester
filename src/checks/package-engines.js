5cf99f8260ae501e8f87d8f6c9e2eaa034719781
Michal Raczka <michal@raczka.me>
2018-03-09 10:46:48 +0100

    Upgrade engines

--- src/checks/package-engines.js
+++ src/checks/package-engines.js
@@ -4,22 +4,22 @@ module.exports = function checkPackageEngines() {
 
   const packageJson = JSON.parse(fs.readFileSync(`${process.cwd()}/package.json`, "utf-8"));
   const { yarn, npm, node } = packageJson.engines;
-  if (node === "6.12.x"
+  if (node === "6.13.x"
     && npm === "3.10.x"
-    && yarn === "1.3.x") {
+    && yarn === "1.5.x") {
     return true;
   }
 
-  if (node === "8.9.x"
-    && npm === "5.5.x"
-    && yarn === "1.3.x") {
+  if (node === "8.10.x"
+    && npm === "5.6.x"
+    && yarn === "1.5.x") {
     return true;
   }
 
   return `\`engines\` param has following value:
   ${JSON.stringify(packageJson.engines)},
   should be:
-  {"node": "6.12.x","npm": "3.10.x","yarn": "1.3.x"}
+  {"node": "6.13.x","npm": "3.10.x","yarn": "1.5.x"}
   or
-  {"node": "8.9.x","npm": "5.5.x","yarn": "1.3.x"}`;
+  {"node": "8.10.x","npm": "5.6.x","yarn": "1.5.x"}`;
 };
